80fbc0eed4036ee8f99aab2cdee665e7bf85eebb
Emil-Deleon <111036019+Emil-Deleon@users.noreply.github.com>
2022-12-14 18:52:38 -0500

    Server.js Complete

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const usersRouter = require("./routes/users");
+const showsRouter = require("./routes/shows");
+const {db} = requqire("./db");
+
+app.get("/", (req, res) => {
+    res.send("Movie Theater: Successful GET request made.");
+});
+
+app.use("/users", usersRouter);
+app.use("/shows", showsRouter);
+
+const port = 3000;
+app.listen(port, () => {
+    db.sync();
+    console.log(`Movie Teatre API is live at https://localhost${port}`);
+})
